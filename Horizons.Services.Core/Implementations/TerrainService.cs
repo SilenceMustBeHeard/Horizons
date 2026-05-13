bd1caf78642afea6f17dafe50c7f7c59df36ab08
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-05-13 21:15:52 +0300

    Added Terrain service

--- Horizons.Services.Core/Implementations/TerrainService.cs
+++ Horizons.Services.Core/Implementations/TerrainService.cs
@@ -1,5 +1,5 @@
 ﻿using Horizons.Data;
-
+using Horizons.Data.Repositories.Interfaces.Base;
 using Horizons.Services.Core.Interfaces;
 using Horizons.Web.ViewModels.Destination;
 using Microsoft.EntityFrameworkCore;
@@ -8,16 +8,23 @@ namespace Horizons.Services.Core.Implementations
 {
     public class TerrainService : ITerrainService
     {
-      
 
+        private readonly ITerrainRepository _terrainRepository;
 
-        public TerrainService(AppDbContext dbContext)
+        public TerrainService(ITerrainRepository terrainRepository)
         {
-           
+            _terrainRepository = terrainRepository;
         }
         public async Task<IEnumerable<AddDestinationTerrainDropdownModel>> GetAllTerrainsDropdownAsync()
         {
-            
+            return await _terrainRepository.Query()
+                .Where(t => !t.IsDeleted)
+                .Select(t => new AddDestinationTerrainDropdownModel
+                {
+                    Id = t.Id,
+                    Name = t.Name
+                })
+                .ToListAsync();
         }
     }
 }
