94910eae2e2976c80ff125d608096f96b038cb8e
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-05-21 16:48:52 +0300

    Added ErrorHandlingExtensions

---
+++ Horizons.Web.Infrastructure/Extensions/ErrorHandlingExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Horizons.Web.Infrastructure.Extensions;
+
+
+public static class ErrorHandlingExtensions
+{
+    public static IApplicationBuilder UseCustomErrorHandling(this IApplicationBuilder app)
+    {
+        return app.UseExceptionHandler(errorApp =>
+        {
+            errorApp.Run(async context =>
+            {
+                context.Response.ContentType = "text/html";
+
+                var exceptionHandlerPathFeature =
+                    context.Features.Get<IExceptionHandlerPathFeature>();
+
+                var exception = exceptionHandlerPathFeature?.Error;
+
+
+
+                // Redirect to custom error page
+                context.Response.Redirect("/Error/500");
+            });
+        });
+    }
+}
