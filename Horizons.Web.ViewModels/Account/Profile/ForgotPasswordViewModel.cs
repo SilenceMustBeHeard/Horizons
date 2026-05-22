6a0b9247eeb14830466b954412c2a310497ed1e9
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-05-22 06:11:14 +0300

    Add account controller, auth views, and styles

---
+++ Horizons.Web.ViewModels/Account/Profile/ForgotPasswordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Horizons.Web.ViewModels.Account.Profile;
+
+
+public class ForgotPasswordViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+}
