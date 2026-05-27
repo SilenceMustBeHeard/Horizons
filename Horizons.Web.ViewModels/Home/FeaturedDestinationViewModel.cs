dc433b90607c265fa79b6f7e2f3214a63d9845e4
SilenceMustBeHeard <spiritofsilence55@gmail.com>
2026-05-27 08:55:31 +0300

    Refactored home page and cleaned useless js

---
+++ Horizons.Web.ViewModels/Home/FeaturedDestinationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Horizons.Web.ViewModels.Home;
+
+public class FeaturedDestinationViewModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public string? Country { get; set; }
+    public string? Description { get; set; }
+    public int Likes { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string? TerrainName { get; set; }
+}
---
+++ Horizons.Web.ViewModels/Home/HomeIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Horizons.Web.ViewModels.Destination;
+
+namespace Horizons.Web.ViewModels.Home
+{
+
+    public class HomeIndexViewModel
+    {
+        public int TotalDestinations { get; set; }
+        public int TotalCountries { get; set; }
+        public int TotalStories { get; set; }
+
+        public int MountainsCount { get; set; }
+        public int BeachesCount { get; set; }
+        public int ForestsCount { get; set; }
+        public int UrbanCount { get; set; }
+        public int DesertsCount { get; set; }
+        public int LakesCount { get; set; }
+
+        public List<FeaturedDestinationViewModel> FeaturedDestinations { get; set; } = new();
+    }
+}
